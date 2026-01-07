85886577539167345e4c38fa9825dc4ce59c0806
Santhosh030208 <santhosh.030208@gmail.com>
2026-01-07 17:06:56 +0530

    Routerfix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import Login from './Pages/Login';
 
 function App() {
   return (
