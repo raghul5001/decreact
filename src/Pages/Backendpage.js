80667daa664bd21539559535a5562abc26bac582
Riddhi Joshi <riddhijoshi.enspirittech@gmail.com>
2026-01-08 10:13:53 +0530

    backend page for 3 cards.

---
+++ src/Pages/Backendpage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Backendpage.scss"
+function Backendpage() {
+  return (
+    <div>
+      <div>
+      <div>
+        <h2>Backend 1</h2>
+      </div>
+      <div>
+        <h2>Backend 2</h2>
+      </div>
+      <div>
+        <h2>Backend 3</h2>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Backendpage
