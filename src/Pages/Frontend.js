539b9e37f19b1151e7511ba2cdc1cc5b7d45a1d0
Nikitha-Thiru <tnikitha10062006@gmail.com>
2026-01-09 22:16:45 +0530

    Modifications applied

--- src/Pages/Frontend.js
+++ src/Pages/Frontend.js
@@ -1,24 +1,45 @@
 import React from 'react'
 import './Frontend.scss'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
 
 
 function Frontend() {
+
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    swipe: true,
+  };
+
   return (
-    <div>
 
-        <div className='HTML'>
+    <div className='body'>
+    <div className="slider-container">
+
+      <Slider {...settings}>
+        <div className="box html-box">
           <h2 className='h'>HTML</h2>
-          <p>Content</p>
+          <FaHtml5 className='Ht' />
+          <p className='hp'>Structure your web pages with semantic elements.</p>
         </div>
 
-
-
-        <div className='c'>
-          <h2 className='C'>CSS</h2>
-          <p>Content</p>
+        <div className="box css-box">
+          <h2 className='c'>CSS</h2>
+          <FaCss3Alt className='Cs' />
+          <p className='cp'>Style with Flexbox, Grid layouts, animations, and transitions. Use variables for theming, media queries for responsiveness. Hover to see interactive scaling.</p>
         </div>
-
+      </Slider>
     </div>
+    </div>
+
   )
 }
 
