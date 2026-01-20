bff4de7f4fa777d3c24a2af06cc7a51f981e3620
Santhosh030208 <santhosh.030208@gmail.com>
2026-01-20 14:05:24 +0530

    login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import "./Login.scss"
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import logo from "./Images/Greenslogo.jpg";
 
 function Login() {
+
+  const[uname, setUname] = useState("")
+  const[pass, setPass] = useState("")
+
+  const handleLogin = (e)=>{
+    e.preventDefault()
+    if(uname == "" || pass == ""){
+      toast.error("Please fill all the details")
+    }
+    else{
+      localStorage.setItem("user", uname)
+      localStorage.setItem("pass", pass)
+      toast.success("Login Successfull")
+
+      setTimeout(() => {
+        window.location.href = "/landingpage"
+      }, 5000);   
+    }
+  }
+
   return (
-    <div>
-        <h1>This is Login Page</h1>
+    <div className='row'>
+      <ToastContainer/>
+      <div className='col-5 left'>
+        <div className='leftcontent'>
+          {/* <img className="logo" src={logo} alt='logo'/> */}
+          <h1>Login</h1>
+          <h3>Get access to your orders, wishlist and recommendations</h3>
+        </div>
+      </div>
+      <div className='col-7 right'>
+        <div className='graytext'>
+          <input className="em" type='text' value={uname} placeholder='Enter Email/Mobile number' onChange={(e)=>setUname(e.target.value)}/><br/>
+          <input className='ep' type='password' value={pass} placeholder='Enter Password' onChange={(e)=>setPass(e.target.value)}/>
+          <h6 className='forgot'>Forgot?</h6>
+          <h6 className='bc'>By continuing, you agree to Paperwall <span style={{color: "blue"}}> Terms of use</span> and <span style={{color: "blue"}}> Privacy policy</span></h6>
+          <button className='login' onClick={handleLogin}><h5>Login</h5></button>
+          <h6 className='dh'><span style={{color: "gray"}}> Don't have an account? </span> <span style={{color: "blue"}}> Register Now </span></h6>
+        </div>
+      </div>
     </div>
   )
 }
